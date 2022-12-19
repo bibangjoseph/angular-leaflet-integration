226c5665ac3d2ee21d7c730954da60d617be2515
BIBANG BEFENE Joseph Donovan <bibangjose@gmail.com>
2022-12-19 09:12:03 +0100

    updates

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,24 +7,20 @@ import * as L from 'leaflet';
 	styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit {
-	private map: any;
+	public map: any;
+
+	private initMap(): void {
+		this.map = L.map('map', {
+			center: [39.8282, -98.5795],
+			zoom: 3
+		});
+	}
+
+	constructor() { }
 
 	ngAfterViewInit(): void {
 		this.initMap();
 	}
 
-	private initMap(): void {
-		this.map = L.map('mapbox', {
-			center: [-0.803689, 11.609444],
-			zoom: 8
-		});
 
-		const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			maxZoom: 18,
-			minZoom: 3,
-			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-		});
-
-		tiles.addTo(this.map);
-	}
 }
